2384f8877062c67b5ee17665e641d7b5ecabe19a
“dongyuekai” <457545648@qq.com>
2021-05-08 10:16:16 +0800

    react-use库的简单测试

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import MediaQuery from 'react-responsive'
 import tools from 'dyk-webpack-build-tools'
+import { useTitle, useUpdate } from 'react-use'
+
 export default () => {
   useEffect(() => {
     console.log('dyk--', tools.math.add(1, 3))
@@ -8,7 +10,14 @@ export default () => {
     console.log('dyk--REACT_APP_URL_API---', process.env.REACT_APP_URL_API)
   }, [])
   const PCHome = () => {
-    return '我是PCHome'
+    const update = useUpdate()
+    useTitle('PCHome页面')
+    return (
+      <div>
+        <div>Time: {Date.now()}</div>
+        <button onClick={update}>Update</button>
+      </div>
+    )
   }
   const MobielHome = () => {
     return '我是MobieHome'
